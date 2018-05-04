83b36463da5549cc0f7be77571820f3620a395bc
leahycarlos21 <leahyvasquez@gmail.com>
2018-05-03 20:34:02 -0600

    Commit necesario para agregar la interfaz

--- Odyssey Cliente/Program.cs
+++ Odyssey Cliente/Program.cs
@@ -6,6 +6,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Dynamic;
+using System.Xml.Linq;
+using System.IO;
 
 namespace Odyssey_Cliente
 {
@@ -18,42 +25,74 @@ namespace Odyssey_Cliente
         static void Main()
         {
             // connect to server
-            TcpClient client = new TcpClient("localhost", 5000);
+            //TcpClient client = new TcpClient("localhost", 5000);
 
-            // Console.Write("Enter name : ");
 
             //String name = Console.ReadLine();
             String name = "Larry";
+            Int32 age = 30;
+            String city = "Copenhagen";
+            String country = "Denmark";
+            //https://stackoverflow.com/questions/11492705/how-to-create-xml-document-using-xmldocument
+            XElement xmlCliente = new XElement("DATA",
+                new XAttribute("ID", 1),
+                new XElement("Field",
+                    new XAttribute("Name", "Name"),
+                    name),
+                new XElement("Field",
+                    new XAttribute("Name", "Age"),
+                    age),
+                new XElement("Field",
+                    new XAttribute("Name", "City"),
+                    city),
+                new XElement("Field",
+                    new XAttribute("Name", "Country"),
+                    country)
+            );
 
-            // send name to server
-            byte[] buf;
-            // append newline as server expects a line to be read
-            buf = Encoding.UTF8.GetBytes(name + "\n");
-
-            NetworkStream stream = client.GetStream();
-            stream.Write(buf, 0, name.Length + 1);
-
-            // read xml from server
-
-            buf = new byte[100];
-            stream.Read(buf, 0, 100);
-            string xml = Encoding.UTF8.GetString(buf);
-            // take only upto first null char
-            xml = xml.Substring(0, xml.IndexOf(char.ConvertFromUtf32(0)));
-
-            XmlDocument doc = new XmlDocument();
-            doc.LoadXml(xml);
-
-            if (doc.DocumentElement.Name == "error")
-                Console.WriteLine("Name not found!");
-            else
+            Console.WriteLine(xmlCliente);
+            using (var clienteSocket = new TcpClient())
             {
-                Console.WriteLine("Mobile : {0}", doc.SelectSingleNode("//mobile").InnerText);
-                Console.WriteLine("Email  : {0}", doc.SelectSingleNode("//email").InnerText);
+                clienteSocket.Connect("localhost", 5000);
+                using (var ns = clienteSocket.GetStream())
+                using (var writer = new StreamWriter(ns))
+                {
+                    writer.Write(xmlCliente);
+                  //  writer.Write("\r\n\r\n");
+                    writer.Flush();
+                }
+                clienteSocket.Close();
             }
         
-    
-    Application.EnableVisualStyles();
+                /*  // send name to server
+                  byte[] buf;
+                  // append newline as server expects a line to be read
+                  buf = Encoding.UTF8.GetBytes(xmlCliente + "\n");
+
+                  NetworkStream stream = client.GetStream();
+                  stream.Write(buf, 0, name.Length + 1);
+
+                  // read xml from server
+
+                  buf = new byte[1000];
+                  stream.Read(buf, 0, 100);
+                  string xml = Encoding.UTF8.GetString(buf);
+                  // take only upto first null char
+                  xml = xml.Substring(0, xml.IndexOf(char.ConvertFromUtf32(0)));
+
+                 /* XmlDocument doc = new XmlDocument();
+                  doc.LoadXml(xml);
+
+                  if (doc.DocumentElement.Name == "error")
+                      Console.WriteLine("Name not found!");
+                  else
+                  {
+                      Console.WriteLine("Mobile : {0}", doc.SelectSingleNode("//mobile").InnerText);
+                      Console.WriteLine("Email  : {0}", doc.SelectSingleNode("//email").InnerText);
+                  }*/
+
+
+                Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
