24dd625713d89c969bcac58899dc121151417ac5
Kevin Guzman <kevin.guzman.navarro@gmail.com>
2018-05-20 22:10:08 -0600

    Encriptar la contrasena
    Se empleo y mejoro el metodo de encriptar la contrasena para que este sea más eficiente .

--- Prueba/Prueba/Form3.cs
+++ Prueba/Prueba/Form3.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
 
 namespace Prueba
 {
@@ -76,8 +77,23 @@ namespace Prueba
 
         private void textBox5_TextChanged(object sender, EventArgs e)
         {
-            password = textBox5.Text;
+            password = EncryptPasswords(textBox5.Text);
 
+        }
+
+        public String EncryptPasswords(String text)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
+            byte[] result = md5.Hash;
+            StringBuilder str = new StringBuilder();
+            for (int i = 1 ; i < result.Length; i++)
+            {
+                str.Append(result[i].ToString("x2"));
+
+
+            }
+            return str.ToString();
         }
     }
 }
